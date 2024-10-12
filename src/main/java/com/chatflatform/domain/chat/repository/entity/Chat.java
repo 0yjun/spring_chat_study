838a2490312fb54efd266c8d66cf611dfa932ae4
jun <an0yjun@naver.com>
2024-10-12 15:38:55 +0900

    완강

--- src/main/java/com/chatflatform/domain/chat/repository/entity/Chat.java
+++ src/main/java/com/chatflatform/domain/chat/repository/entity/Chat.java
@@ -10,7 +10,7 @@ import java.sql.Timestamp;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name="user")
+@Table(name="chat")
 public class Chat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
